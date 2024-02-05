3671da86bfd74ff39d31edd88ce97a46b3867273
Abdimajid2 <147602028+Abdimajid2@users.noreply.github.com>
2024-02-05 15:30:30 +0100

    first push

---
+++ WeatherData/WeatherData/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace WeatherData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rowcount = 0;
+
+            var matches =  CollectData.ReadAll("tempdata5-med fel.txt");
+            foreach (var match in matches)
+            {
+
+
+                foreach (var item in match)
+                {
+
+                    Console.WriteLine(rowcount + " " + item);
+                    rowcount++;
+
+                }
+
+            }
+                        
+        }
+    }
+}
